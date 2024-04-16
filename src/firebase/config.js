0085cda5001771c2ce7d67b29b653a45c04297f2
Omar Youssef <73360852+ommrahmdd@users.noreply.github.com>
2024-04-16 17:50:17 +0200

    feat: Initiate firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYFUU4utqqiZd_sdmJUjR-qcPLIULDGHU",
+  authDomain: "photography-portfolio-862d8.firebaseapp.com",
+  projectId: "photography-portfolio-862d8",
+  storageBucket: "photography-portfolio-862d8.appspot.com",
+  messagingSenderId: "298132243551",
+  appId: "1:298132243551:web:393406ce16972cab6cc7ea",
+};
+
+firebase.initializeApp(firebaseConfig);
